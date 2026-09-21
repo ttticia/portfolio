3fb89da5ab375a9ce75c22e690298517d6a472c7
Tricia ng <ng.tricia2005@gmail.com>
2026-09-21 21:46:02 +0800

    Fix workflow extension and deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Set this to your production URL (no trailing slash)
-  site: 'https://ttticia.github.io',
+  site: 'github.com/ttticia/portfolio',
   // Set this to your site's subpath if it is NOT hosted at the domain root
   // (e.g. GitHub Pages project sites). Delete this line if your site lives at "/".
   base: '/portfolio',
